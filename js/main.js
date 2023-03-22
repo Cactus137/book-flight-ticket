05ca09202e35619e704ca3cf5e161056436451b4
Tnenl <74536734+Tnenl@users.noreply.github.com>
2023-03-22 14:22:38 +0700

    asm1

---
+++ js/main.js
@@ -0,0 +1,9 @@
+function trangchu(){
+    alert('Chào mừng đến với Expedia')
+}
+function datve(){
+    alert('Xin chào! Đây là trang đặt vé')
+}
+function check(){
+    alert('Đặt vé thành công!')
+}
